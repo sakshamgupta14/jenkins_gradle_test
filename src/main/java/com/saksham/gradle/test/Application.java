d264b01fd67efa7fa79a1419d76b9050c868b627
saksham <saksham.gupta@tothenew.com>
2017-10-25 21:06:28 +0530

    setup done

---
+++ src/main/java/com/saksham/gradle/test/Application.java
@@ -0,0 +1,20 @@
+package com.saksham.gradle.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * Created by saksham on 25/10/17.
+ */
+
+@SpringBootApplication
+@ComponentScan("com.saksham.gradle.test")
+@EnableWebMvc
+public class Application {
+
+  public static void main(String args[]) {
+    SpringApplication.run(Application.class);
+  }
+}
